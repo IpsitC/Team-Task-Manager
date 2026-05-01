2ed10a4da9def64f37262edae76c5b76a1e1abc8
Ipsitneel Chaudhuri <ipsit.chaudhuri@gmail.com>
2026-05-01 23:37:59 +0530

    chore: update config for railway deployments

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,5 +5,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173
+  },
+  preview: {
+    allowedHosts: [
+      'frontend-production-ce95.up.railway.app',
+      'ttaskmanager.up.railway.app'
+    ]
   }
 });
